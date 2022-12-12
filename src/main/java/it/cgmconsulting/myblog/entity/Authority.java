de8368c99b34217ab98f70af3061600cfc8f4017
CiccioFr <cicciofr.net@tiscali.it>
2022-12-12 18:05:08 +0100

    Entity Authority

---
+++ src/main/java/it/cgmconsulting/myblog/entity/Authority.java
@@ -0,0 +1,39 @@
+package it.cgmconsulting.myblog.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true, length = 25)
+    private String authorityName;
+
+    public Authority(String authorityName) {
+        this.authorityName = authorityName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Authority authority = (Authority) o;
+        return id == authority.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
