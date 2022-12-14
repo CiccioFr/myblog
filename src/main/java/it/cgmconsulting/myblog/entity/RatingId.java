ae0a4c942e76413e9b882518840ce361fb401a60
CiccioFr <cicciofr.net@tiscali.it>
2022-12-14 21:40:38 +0100

    creata Class RatingId per Primary Key Composta

---
+++ src/main/java/it/cgmconsulting/myblog/entity/RatingId.java
@@ -0,0 +1,45 @@
+package it.cgmconsulting.myblog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Gestione della Primary Key composta
+ */
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatingId implements Serializable {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private User post;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RatingId ratingId = (RatingId) o;
+        return Objects.equals(user, ratingId.user) && Objects.equals(post, ratingId.post);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, post);
+    }
+}
