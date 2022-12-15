546222a68f32b05f6888f14ec950c319f2db83f6
CiccioFr <cicciofr.net@tiscali.it>
2022-12-15 11:00:13 +0100

    agg Package REPOSITORY per implement Spring Security

---
+++ src/main/java/it/cgmconsulting/myblog/repository/AuthorityRepository.java
@@ -0,0 +1,23 @@
+package it.cgmconsulting.myblog.repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+import it.cgmconsulting.myblog.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority , Long>{
+
+	Optional<Authority> findByAuthorityName(String authorityName);
+
+	boolean existsByAuthorityName(String authorityName);
+
+	Set<Authority> findByIdIn(Set<Long> ids);
+
+
+
+
+}
---
+++ src/main/java/it/cgmconsulting/myblog/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package it.cgmconsulting.myblog.repository;
+
+import java.util.Optional;
+
+import it.cgmconsulting.myblog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByEmail(String email);
+
+	Optional<User> findByUsernameOrEmail(String username, String email);
+
+	Optional<User> findByUsername(String username);
+
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
+
+}
