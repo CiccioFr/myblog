37efae5a090a9a901409a1ddc2a216eb07e22bc7
CiccioFr <cicciofr.net@tiscali.it>
2022-12-15 10:53:00 +0100

    Class User aggiunti 2 costruttori per implementare Spring Security

--- src/main/java/it/cgmconsulting/myblog/entity/User.java
+++ src/main/java/it/cgmconsulting/myblog/entity/User.java
@@ -50,6 +50,20 @@ public class User extends CreationUpdate {
         this.email = email;
     }
 
+    public User(long id, String username, String email, Set<Authority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.authorities = authorities;
+    }
+
+    public User(String username, String email, String password, Set<Authority> authorities) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
