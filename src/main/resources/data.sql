42c9465cb60a03fec545b2e70645d127e9bc48c1
CiccioFr <cicciofr.net@tiscali.it>
2023-01-09 11:17:59 +0100

    data.sql tabelle di dominio

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,5 @@ INSERT IGNORE INTO authority VALUE (2, 'ROLE_EDITOR');
 INSERT IGNORE INTO authority VALUE (3, 'ROLE_READER');
 INSERT IGNORE INTO authority VALUE (4, 'ROLE_MODERATOR');
 INSERT IGNORE INTO authority VALUE (5, 'ROLE_GUEST');
+
+-- tabelle di dominio, che non hanno ForeignKey
