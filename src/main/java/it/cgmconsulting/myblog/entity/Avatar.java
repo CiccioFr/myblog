d7d9e790f070bad7192b3c806fb99ffcc153ce05
CiccioFr <cicciofr.net@tiscali.it>
2023-01-09 15:15:54 +0100

    Avatar

--- src/main/java/it/cgmconsulting/myblog/entity/Avatar.java
+++ src/main/java/it/cgmconsulting/myblog/entity/Avatar.java
@@ -1,2 +1,43 @@
-package it.cgmconsulting.myblog.entity;public class Avatar {
+package it.cgmconsulting.myblog.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity @Getter @Setter @NoArgsConstructor
+public class Avatar {
+    // per scrivere una immagine su db servono essenzialmente 3 valori
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String filename;
+
+    // il mime tipe associato (NON L'ESTENSIONE) al file es.: "image/jpg", "image/gif", "image/png"
+    @Column(nullable = false)
+    private String filetype;
+
+    // array di byte: come viene salvato il file su DB
+    @Column(nullable = false)
+    // sul db diventa un tipo C-Lob
+    @Lob
+    private byte[] data;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Avatar avatar = (Avatar) o;
+        return id == avatar.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
